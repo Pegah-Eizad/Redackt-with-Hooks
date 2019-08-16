11b54073221315a929d7e5db6491f7cdd6574b33
Pegah Eizadkhah <peizadkh@gmail.com>
2019-08-16 17:02:03 -0400

    adds sidebar actions

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,7 @@ import add from '../images/add-icon.svg';
 import remove from '../images/remove-icon.svg';
 import message from '../images/message-icon-2.svg';
 import unread from '../images/unread-icon.svg';
+import { getPosts } from '../store/actions/Sidebar.Actions.js'
 
 export default class Sidebar extends Component {
 	state = {
@@ -137,13 +138,14 @@ export default class Sidebar extends Component {
 		}
 	}
 
-	changeActiveSub = (name) => {
-		this.setState(prevState => {
-		  return {
-			activeSub: name,
-			activeSubURL: "https://www.reddit.com/r/" + name + "/"+this.state.sortType+".json?limit=10&raw_json=1"
-		  }
-		});
+	updateActiveSub = (name) => {
+		this.props.dispatch(getPosts(subreddit));
+		// this.setState(prevState => {
+		//   return {
+		// 	activeSub: name,
+		// 	activeSubURL: "https://www.reddit.com/r/" + name + "/"+this.state.sortType+".json?limit=10&raw_json=1"
+		//   }
+		// });
 	}
 
 	componentWillMount() {
@@ -209,7 +211,7 @@ export default class Sidebar extends Component {
 						  return(
 							<li
 								className={(subReddit.name === this.state.activeSub) ? 'active' : ''}
-								onClick={() => this.changeActiveSub(subReddit.name)}
+								onClick={() => this.updateActiveSub(subReddit.name)}
 								key={subReddit.id.toString()} >
 								<span># {subReddit.name}</span>
 								<span className={"remove-button"}>
