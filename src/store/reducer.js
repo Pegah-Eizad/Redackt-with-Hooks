4b433fa886dcfc4964e1a1742ebe09be20d2de24
Pegah Eizad <pegaheizad@Pegahs-MacBook-Air.local>
2019-08-30 15:13:27 -0400

    modified initial state

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,43 +4,49 @@ import RedditPost from '../components/RedditPost.js';
 
 const initialState = {
     state = {
-		subreddits: [
-			{
-			  name: "AskReddit",
-			  isStarred: false,
-			  id: 0
-			},
-			{
-			  name: "all",
-			  isStarred: false,
-			  id: 1
-			},
-			{
-			  name: "RocketLeague",
-			  isStarred: false,
-			  id: 2
-			},
-			{
-			  name: "pics",
-			  isStarred: false,
-			  id: 3
-			},
-			{
-			  name: "reactjs",
-			  isStarred: false,
-			  id: 4
-			},
-			{
-			  name: "videos",
-			  isStarred: false,
-			  id: 5
+			activeSub: "AskReddit",
+			activeSubURL: "https://www.reddit.com/r/askreddit/top.json?limit=10&raw_json=1",
+			sortType: "top",
+			sidebar: {
+				subreddits: [
+				{
+				name: "AskReddit",
+				isStarred: false,
+				id: 0
+				},
+				{
+				name: "all",
+				isStarred: false,
+				id: 1
+				},
+				{
+				name: "RocketLeague",
+				isStarred: false,
+				id: 2
+				},
+				{
+				name: "pics",
+				isStarred: false,
+				id: 3
+				},
+				{
+				name: "reactjs",
+				isStarred: false,
+				id: 4
+				},
+				{
+				name: "videos",
+				isStarred: false,
+				id: 5
+				}
+			],
+			subredditInput: "",
+			subredditInputHasFocus: false,
+		    },
+			mainBody: {
+				isActiveSubStarred=false,
+				getDarkMode: false
 			}
-		  ],
-		subredditInput: "",
-		subredditInputHasFocus: false,
-		activeSub : "AskReddit",
-		sortType: "top",
-		activeSubURL: "https://www.reddit.com/r/askreddit/top.json?limit=10&raw_json=1"
     }
 };
 
