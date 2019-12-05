280d57c832e1b349676b341318aa5ba8938fdc38
Pegah Eizad <peizadkh@gmail.com>
2019-12-05 16:02:31 -0500

    removed state from mainbody

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -6,53 +6,54 @@ import atIcon from "../images/at-icon.svg";
 
 export default class MainBody extends Component {
 
-    state = {
-      subCount: 100,
-      isToggleOn: false,
-      getMorePosts: false
-    };
+    // state = {
+    //   subCount: 100,
+    //   isToggleOn: false,
+    //   getMorePosts: false
+    // };
 
     //Function to get the subscriber count of the subreddit.
-    getSubCount = (subCount) => {
-      if (subCount > 999999) {
-          subCount = (subCount/1000000).toFixed(1) + 'M';
-      } else if (subCount > 999) {
-          subCount = (subCount/1000).toFixed(1) + 'k';
-      } else {
-          subCount = subCount + 0;
-      }
-      this.setState(prevState => {
-        return {
-            subCount: subCount > 999 ? (subCount/1000).toFixed(1) + 'k' : subCount
-        }
-      })
-    };
+    // getSubCount = (subCount) => {
+    //   if (subCount > 999999) {
+    //       subCount = (subCount/1000000).toFixed(1) + 'M';
+    //   } else if (subCount > 999) {
+    //       subCount = (subCount/1000).toFixed(1) + 'k';
+    //   } else {
+    //       subCount = subCount + 0;
+    //   }
+    //   this.setState(prevState => {
+    //     return {
+    //         subCount: subCount > 999 ? (subCount/1000).toFixed(1) + 'k' : subCount
+    //     }
+    //   })
+    // };
 
     // Function to open thread
-    isThreadOpen = () => {
-        this.setState(prevState => ({
-            isToggleOn: true
-        }));
-    };
+    // isThreadOpen = () => {
+    //     this.setState(prevState => ({
+    //         isToggleOn: true
+    //     }));
+    // };
 
-    // Function to open thread
-    isThreadClose = () => {
-        this.setState(prevState => ({
-            isToggleOn: false
-        }));
-    };
+    // // Function to open thread
+    // isThreadClose = () => {
+    //     this.setState(prevState => ({
+    //         isToggleOn: false
+    //     }));
+    // };
 
-    //Get more posts in main body.
-    getMorePosts = () => {
-        this.setState(prevState => ({
-            getMorePosts: !prevState.getMorePosts
-        }));
-    };
+    // //Get more posts in main body.
+    // getMorePosts = () => {
+    //     this.setState(prevState => ({
+    //         getMorePosts: !prevState.getMorePosts
+    //     }));
+    // };
 
   render() {
-    console.log('Mainbody :::', this.props);
+    console.log('Mainbody :::', this.props.activeSubURL);
     return (
-      <div className={!this.state.isToggleOn ? 'main thread-is-closed' : 'main thread-is-open'}>
+    //   <div className={!this.state.isToggleOn ? 'main thread-is-closed' : 'main thread-is-open'}>
+    <div className={'main thread-is-open'}>
         <Header
             activeSub={this.props.activeSub}
             activeSubURL={this.props.activeSubURL}
@@ -86,7 +87,7 @@ export default class MainBody extends Component {
                     <div className="message-bar-input">
                         <label className="tooltip">
                             <span className="tooltiptext tooltip-top">Typing here doesn't do anything yet, sorry.</span>
-                            <input type="text" placeholder={"Message #" + this.props.activeSub}
+                            {/* <input type="text" placeholder={"Message #" + this.props.activeSub}
                                    defaultValue={this.state.searchInput}
                                    className="searchInput"
                                    onChange={this.handleChange}
@@ -94,6 +95,9 @@ export default class MainBody extends Component {
                                    onFocus={this.handleFocus}
                                    onBlur={this.handleBlur}
                                    ref={el => (this.searchInput = el)}
+                            /> */}
+                            <input type="text" placeholder={"Message #" + this.props.activeSub}
+                                   className="searchInput"
                             />
                             <img src={atIcon} alt="at-icon"/>
                         </label>
